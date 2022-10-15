cde0a313e4e4374a3ff7918d2ae282fb267872ce
Azeez <Azeez Mohammed>
2022-10-14 17:16:33 -0700

    task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <unisted.h>
+/**
+ * main - Entry poin
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int size;
+	int standard_erro = 2;
+
+	size =
+		sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	write(standard_error,
+			"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+			size);
+	return (1);
+}
