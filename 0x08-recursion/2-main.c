7886b48f2ada6e92619a24d16f3ce1e9764c0936
Azeez <Azeez Mohammed>
2022-11-03 14:57:28 -0700

    3-main.c

---
+++ 0x08-recursion/2-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+Return: Always 0.
+{
+	 int n;
+	 
+	 n = _strlen_recursion("Corbin Coleman");
+printf("%d\n", n);
+return (0);
+}
